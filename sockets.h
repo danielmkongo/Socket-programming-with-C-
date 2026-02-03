370b12e8ea28d50ee1be47f21ebe169421dbbe7f
Daniel Mkongo <77916192+danielmkongo@users.noreply.github.com>
2026-02-03 10:02:58 +0300

    Update sockets.h

--- sockets.h
+++ sockets.h
@@ -1,180 +1,141 @@
 #pragma once
-#include<WinSock2.h>
-#include<WS2tcpip.h>
-#include<iostream>
+#include <WinSock2.h>
+#include <WS2tcpip.h>
+#include <iostream>
+#include <string>
+
+#pragma comment(lib, "ws2_32.lib")
 
 constexpr size_t MAXDATALENGTH = 4096;
 
-#pragma comment(lib,"ws2_32.lib")
-#pragma warning(disable:4996)
-
 class Socket {
 private:
-	SOCKET Listen_sock, Work;
-	sockaddr_in address;
-	sockaddr_in clientaddress;
-	int addlen;
-	bool status;
-	uint16_t port;
+    SOCKET listenSock = INVALID_SOCKET;
+    SOCKET workSock   = INVALID_SOCKET;
+    sockaddr_in address{};
+    sockaddr_in clientAddress{};
+    int addrLen = sizeof(sockaddr_in);
+    bool connected = false;
+    uint16_t port = 0;
 
-private:
-	void configure(const short& port) {
-		this->address.sin_addr.s_addr = INADDR_ANY;
-		this->address.sin_family = AF_INET;
-		this->port = port;
-		this->address.sin_port = htons(port);
-	}
+    void configureServer(uint16_t p) {
+        port = p;
+        address.sin_family = AF_INET;
+        address.sin_addr.s_addr = INADDR_ANY;
+        address.sin_port = htons(port);
+    }
 
-	void configure(const unsigned& port, const char* Address) {
-		address.sin_addr.s_addr = inet_addr(Address);
-		address.sin_family = AF_INET;
-		address.sin_port = htons(port);
-	}
+    void configureClient(uint16_t p, const char* ip) {
+        port = p;
+        address.sin_family = AF_INET;
+        inet_pton(AF_INET, ip, &address.sin_addr);
+        address.sin_port = htons(port);
+    }
 
-	void operator=(const SOCKET& other) {
-		this->Work = other;
-	}
 public:
+    /* ===== INIT ===== */
+    Socket() {
+        static bool wsaInit = false;
+        if (!wsaInit) {
+            WSADATA data;
+            if (WSAStartup(MAKEWORD(2, 2), &data) != 0) {
+                throw std::runtime_error("WSAStartup failed");
+            }
+            wsaInit = true;
+        }
 
-	std::string getClientName() {
-		char host[NI_MAXHOST];
-		char service[NI_MAXSERV];
-		if (!getnameinfo((sockaddr*)&this->clientaddress, sizeof(this->clientaddress),
-			host, NI_MAXHOST, service, NI_MAXSERV, NULL))
-		{
+        workSock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+        if (workSock == INVALID_SOCKET) {
+            throw std::runtime_error("socket() failed");
+        }
+    }
 
-			return host;
-		}
+    ~Socket() {
+        close();
+    }
 
-		inet_ntop(AF_INET, &this->clientaddress.sin_addr, host, NI_MAXHOST);
-		return host;
-	}
+    /* ===== SERVER ===== */
+    bool listen(uint16_t p) {
+        configureServer(p);
 
-	void listen(const short& port) {
-		this->configure(port);
-		this->Listen_sock = socket(AF_INET, SOCK_STREAM, NULL);
-		bind(this->Listen_sock, (sockaddr*)&this->address, sizeof(this->address));
-		::listen(this->Listen_sock, SOMAXCONN);
-	}
+        listenSock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+        if (listenSock == INVALID_SOCKET) return false;
 
-	void listenAsync() {
-		int result=::listen(this->Listen_sock, SOMAXCONN);
-	}
+        if (bind(listenSock, (sockaddr*)&address, sizeof(address)) == SOCKET_ERROR)
+            return false;
 
-	uint16_t getPort() {
-		return this->port;
-	}
+        if (::listen(listenSock, SOMAXCONN) == SOCKET_ERROR)
+            return false;
 
-	void listen() {
-		this->configure(this->port);
-		this->Listen_sock = socket(AF_INET, SOCK_STREAM, NULL);
-		bind(this->Listen_sock, (sockaddr*)&this->address, sizeof(this->address));
-		::listen(Listen_sock, SOMAXCONN);
-	}
+        return true;
+    }
 
-	bool accept(Socket& client) {
-		client = ::accept(this->Listen_sock, (sockaddr*)&this->clientaddress, &addlen);
-		if (client.getSocket() == 0) {
-			throw "error could not establish connection!";
-			return false;
-		}
-		else {
-			return true;
-		}
-	}
+    bool accept(Socket& client) {
+        SOCKET s = ::accept(listenSock, (sockaddr*)&clientAddress, &addrLen);
+        if (s == INVALID_SOCKET) return false;
 
+        client.workSock = s;
+        client.clientAddress = clientAddress;
+        client.connected = true;
+        return true;
+    }
 
-	SOCKET accept() {
-		SOCKET client = ::accept(this->Listen_sock, (sockaddr*)&this->clientaddress, &addlen);
-		if (client == 0) {
-			throw "error could not establish connection!";
-			return 0;
-		}
-		else {
-			return client;
-		}
-	}
+    /* ===== CLIENT ===== */
+    bool connect(const char* ip, uint16_t p) {
+        configureClient(p, ip);
 
-	bool send(const char* data, const size_t& size) {
-		if (::send(this->Work, data, (int)size, NULL)) {
-			return true;
-		}
-		return false;
-	}
+        if (::connect(workSock, (sockaddr*)&address, sizeof(address)) == SOCKET_ERROR)
+            return false;
 
-	bool send(const char* data) {
-		if (::send(this->Work, data, sizeof(data), NULL)) {
-			return true;
-		}
-		return false;
-	}
+        connected = true;
+        return true;
+    }
 
-	bool isConnected() {
-		return status;
-	}
+    /* ===== DATA ===== */
+    bool send(const char* data, size_t size) {
+        return (::send(workSock, data, (int)size, 0) > 0);
+    }
 
-	bool connect(const char* ipaddress, const unsigned& port) {
-		this->configure(port, ipaddress);
-		if (::connect(this->Work, (sockaddr*)&this->address, this->addlen) != 0) {
-			return false;
-		}
-		return true;
-	}
+    bool send(const char* data) {
+        return send(data, strlen(data));
+    }
 
-	bool receive(char* buffer, const size_t& size, size_t& bytes) {
-		bytes = recv(this->Work, buffer,(int)size, NULL);
-		
-		if (bytes > 0 && strlen(buffer) != NULL) {
-			return true;
-		}
+    bool receive(char* buffer, size_t size, size_t& bytes) {
+        bytes = recv(workSock, buffer, (int)size, 0);
 
-		else if (bytes == 0) {
-			std::cout << "\n" << this->getClientName() << "has disconnected!\n";
-			this->status = false;
-			return false;
-		}
-		return false;
-	}
+        if (bytes > 0) {
+            return true;
+        }
+        if (bytes == 0) {
+            connected = false;
+        }
+        return false;
+    }
 
-	void setConnectionStatus(bool status) {
-		this->status = status;
-	}
+    std::string getClientName() {
+        char host[NI_MAXHOST];
+        inet_ntop(AF_INET, &clientAddress.sin_addr, host, NI_MAXHOST);
+        return host;
+    }
 
-	SOCKET getSocket() {
-		return this->Work;
-	}
-	bool setBlocking(bool blocking) {
-		if (this->Work < 0) return false;
+    bool isConnected() const {
+        return connected;
+    }
 
-#ifdef _WIN32
-		unsigned long mode = blocking ? 0 : 1;
-		return (ioctlsocket(this->Work, FIONBIO, &mode) == 0) ? true : false;
-#else
-		int flags = fcntl(fd, F_GETFL, 0);
-		if (flags == -1) return false;
-		flags = blocking ? (flags & ~O_NONBLOCK) : (flags | O_NONBLOCK);
-		return (fcntl(fd, F_SETFL, flags) == 0) ? true : false;
-#endif
-	}
+    SOCKET getSocket() const {
+        return workSock;
+    }
 
-	void close() {
-		closesocket(this->Work);
-		this->setConnectionStatus(false);
-		WSACleanup();
-	}
+    void close() {
+        if (workSock != INVALID_SOCKET) {
+            closesocket(workSock);
+            workSock = INVALID_SOCKET;
+        }
+        connected = false;
+    }
 
-
-	Socket() {
-		WSAData data;
-		if (WSAStartup(MAKEWORD(2, 0), &data) != 0) {
-			std::cerr << "error initiating...!";
-		}
-		memset(this, NULL, sizeof(*this));
-		this->Work = socket(AF_INET, SOCK_STREAM, NULL);
-		this->addlen = sizeof(address);
-		this->status = false;
-	}
-
-	~Socket() {};
-
+    bool setBlocking(bool blocking) {
+        u_long mode = blocking ? 0 : 1;
+        return ioctlsocket(workSock, FIONBIO, &mode) == 0;
+    }
 };
